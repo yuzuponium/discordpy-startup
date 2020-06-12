9cf0a8cb12d48bd18c0d17dcac4163bd07c54e12
yuzuponium <66835213+yuzuponium@users.noreply.github.com>
2020-06-12 23:48:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='?')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
@@ -14,8 +14,8 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def yuuta(ctx):
+    await ctx.send('藤井です')
 
 
 bot.run(token)
