56ddfc6940e0a1d70d23f69fc733f5bc115b283b
yuzuponium <66835213+yuzuponium@users.noreply.github.com>
2020-06-15 12:19:20 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,22 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def yuuta(ctx):
     await ctx.send('いやそれは違うんじゃあないかと。')
+    @bot.command()
+async def urlimg(ctx, *args):
+    message = args[1] 
+    message = message.replace('_',' ')
+    if len(args) == 2:
+        fontcolor = '#FF5555'
+    else:
+        fontcolor = args[2]
+    imgurl = args[0]
+    file_name = C:\Users\藤井　佑太\Pictures\Saved Pictures
+    r = requests.get(imgurl)
+    img = Image.open(io.BytesIO(r.content))
+    img.save(file_name)
+    img ='.1591974976382 .img_add_msg(file_name, message,fontcolor)
+    img.save("./images/result.png")
+    await ctx.send(file=discord.File("./images/result.png"))
 
 
 bot.run(token)
