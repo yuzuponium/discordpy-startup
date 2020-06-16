6902dee7c3a8c44b482b4755a95912c2473755f3
yuzuponium <66835213+yuzuponium@users.noreply.github.com>
2020-06-16 22:29:42 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 import os
 import traceback
-
+import makeyupoline
 bot = commands.Bot(command_prefix='?')
 token = os.environ['DISCORD_BOT_TOKEN']
 
@@ -16,7 +16,13 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def yuuta(ctx):
     await ctx.send('いやそれは違うんじゃあないかと。')
- 
+    
+ @bot.command()
+async def fujii(ctx , msg):
+    
+    img=makeyupoline.img_add_msg("./yupoline.png", msg ,#FF5555,30)
+    img.save("./yupoline_res.png")
+    await ctx.send(file=discord.File("./yupoline_res.png"))
 
 
 bot.run(token)
